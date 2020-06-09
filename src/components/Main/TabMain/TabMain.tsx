c6d773ce5d9a8cf43bb3e35823ef6064b61e35b5
danieldyachenko <daniildyachenko95@gmail.com>
2020-06-09 22:09:30 +0300

    add TabName

--- src/components/Main/TabMain/TabMain.tsx
+++ src/components/Main/TabMain/TabMain.tsx
@@ -1,17 +1,16 @@
 import React from 'react'
-import {MainRoot} from "./MainStyles";
 import Tabs from '@material-ui/core/Tabs';
 import {Tab} from "@material-ui/core";
 import Divider from '@material-ui/core/Divider';
 
-const Main = () => {
+const TabMain = () => {
 
     const [value, setValue] = React.useState(2)
 
     const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => setValue(newValue)
 
     return (
-        <MainRoot>
+        <>
             <Tabs
                 value={value}
                 indicatorColor="primary"
@@ -22,8 +21,8 @@ const Main = () => {
                 <Tab label="Global Feed"/>
             </Tabs>
             <Divider/>
-        </MainRoot>
+        </>
     )
 }
 
-export default Main
+export default TabMain
